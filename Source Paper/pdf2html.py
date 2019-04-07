dcb1b2a8f48b8f0c9ba275fb9dc9e448907bda11
G_MY <30328316+LOMOGMY@users.noreply.github.com>
2019-04-07 16:02:30 +0800

    Add files via upload

---
+++ Source Paper/pdf2html.py
@@ -0,0 +1,27 @@
+import os
+
+os.system('pdf2txt.py -o 1.html 1.pdf')
+
+os.system('pdf2txt.py -o 2.html 2.pdf')
+
+os.system('pdf2txt.py -o 3.html 3.pdf')
+
+os.system('pdf2txt.py -o 4.html 4.pdf')
+
+os.system('pdf2txt.py -o 5.html 5.pdf')
+
+os.system('pdf2txt.py -o 6.html 6.pdf')
+
+os.system('pdf2txt.py -o 7.html 7.pdf')
+
+os.system('pdf2txt.py -o 8.html 8.pdf')
+
+os.system('pdf2txt.py -o 9.html 9.pdf')
+
+os.system('pdf2txt.py -o 10.html 10.pdf')
+
+os.system('pdf2txt.py -o 11.html 11.pdf')
+
+os.system('pdf2txt.py -o 12.html 12.pdf')
+
+os.system('pdf2txt.py -o 13.html 13.pdf')
